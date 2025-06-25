31bf83001fb99a74b62f8f4af9e1cb4031541eed
Muhammad Bilal <140587643+bilalAlgoace@users.noreply.github.com>
2025-06-25 13:44:46 +0500

    updated port for Render deployment

--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 10000;
 httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
